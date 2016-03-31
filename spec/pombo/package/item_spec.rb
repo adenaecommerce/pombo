ad3382f2596f78aee750419d2e306771326cf52b
Leandro Nunes <leandronunes.dev@gmail.com>
2016-03-31 19:49:02 -0300

    add package item

---
+++ spec/pombo/package/item_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe Pombo::Package::Item do
+  shared_examples 'default_value' do |method, value|
+    it "starts with the default value" do
+      expect(subject.send(method)).to equal(value)
+    end
+  end
+
+  shared_examples 'calculate_volume' do |format_title, format, result|
+    it "calculates the volume to the #{ format_title } format" do
+      subject.format = format
+      expect(subject.volume).to equal(result)
+    end
+  end
+
+  describe '#weight' do
+    include_examples 'default_value', :weight, 0.0
+  end
+
+  describe '#length' do
+    include_examples 'default_value', :length, 0.0
+  end
+
+  describe '#height' do
+    include_examples 'default_value', :height, 0.0
+  end
+
+  describe '#width' do
+    include_examples 'default_value', :width, 0.0
+  end
+
+  describe '#diameter' do
+    include_examples 'default_value', :diameter, 0.0
+  end
+
+  describe '#format' do
+    include_examples 'default_value', :format, Pombo::Package::Format::PACKAGE
+
+    it 'throw exception if the format is not supported' do
+      expect{ subject.format = 3434324234 }.to raise_error(TypeError)
+    end
+  end
+
+  describe '#volume' do
+    subject { Pombo::Package::Item.new weight: 10, length: 15, height: 5, width: 10, diameter: 8 }
+
+    include_examples 'calculate_volume', 'package', Pombo::Package::Format::PACKAGE, 750.0
+
+    include_examples 'calculate_volume', 'roll', Pombo::Package::Format::ROLL, 251.33
+
+    include_examples 'calculate_volume', 'envelope', Pombo::Package::Format::ENVELOPE, 0
+  end
+end
