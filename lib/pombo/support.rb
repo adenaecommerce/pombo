6abaf87ea79c5b1961f2adbe7cbecb8855454af8
Leandro Nunes <leandronunes.dev@gmail.com>
2016-04-04 14:02:01 -0300

    adds method in support

--- lib/pombo/support.rb
+++ lib/pombo/support.rb
@@ -2,5 +2,10 @@ module Pombo
   module Support
     TRUE_VALUES  = [true, 1, '1', 't', 'T', 'true', 'TRUE']
     FALSE_VALUES = [false, 0, '0', 'f', 'F', 'false', 'FALSE']
+
+    def self.str_real_to_float(value)
+      raise TypeError, "no implicit conversion of #{ value.class.name } into String" unless value.kind_of? String
+      value.gsub(',','.').to_f
+    end
   end
 end
--- spec/pombo/support_spec.rb
+++ spec/pombo/support_spec.rb
@@ -10,4 +10,14 @@ describe Pombo::Support do
     expect(Pombo::Support::FALSE_VALUES).to eql([false, 0, '0', 'f', 'F', 'false', 'FALSE'])
   end
 
+  describe '.str_real_to_float' do
+    it 'convert real string to float' do
+      expect(subject.str_real_to_float('2,99')).to eq(2.99)
+    end
+
+    it 'throw exception if the value is not string' do
+      expect { subject.str_real_to_float(2) }.to raise_error TypeError
+    end
+  end
+
 end
