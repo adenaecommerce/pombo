bcc02e1014ad60d1282b427c50e4fdb9079382f0
Leandro Nunes <leandronunes.dev@gmail.com>
2016-05-11 19:00:26 -0300

    replaces the i18n by Pombo

--- lib/pombo/package/format.rb
+++ lib/pombo/package/format.rb
@@ -42,9 +42,9 @@ module Pombo
 
       def self.all_formats
         {
-          '1' => OpenStruct.new(code: 1, name: I18n.t('formats.1'), max_length: 105, min_length: 16, max_height: 105, min_height: 2, max_width: 105, min_width: 11, max_dimension: 200),
-          '2' => OpenStruct.new(code: 2, name: I18n.t('formats.2'), max_length: 105, min_length: 18, max_diameter: 91, min_diameter: 5, max_dimension: 200),
-          '3' => OpenStruct.new(code: 3, name: I18n.t('formats.3'), max_length: 60, min_length: 16, max_width: 60, min_width: 11, max_weight: 1),
+          '1' => OpenStruct.new(code: 1, name: Pombo.t('formats.1'), max_length: 105, min_length: 16, max_height: 105, min_height: 2, max_width: 105, min_width: 11, max_dimension: 200),
+          '2' => OpenStruct.new(code: 2, name: Pombo.t('formats.2'), max_length: 105, min_length: 18, max_diameter: 91, min_diameter: 5, max_dimension: 200),
+          '3' => OpenStruct.new(code: 3, name: Pombo.t('formats.3'), max_length: 60, min_length: 16, max_width: 60, min_width: 11, max_weight: 1),
         }
       end
     end
