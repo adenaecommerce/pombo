91c726a6a33763271ea8d7dca388255467060102
Leandro Nunes <leandronunes.dev@gmail.com>
2016-05-14 11:51:35 -0300

    melhora a documentação

--- lib/pombo/support.rb
+++ lib/pombo/support.rb
@@ -4,14 +4,23 @@ module Pombo
     TRUE_VALUES  = [true, 1, '1', 't', 'T', 'true', 'TRUE']
     FALSE_VALUES = [false, 0, '0', 'f', 'F', 'false', 'FALSE']
 
-    # @return [String] the converted string pattern R$ (unit brazilian) to pattern Float. e.g '2,00' to '2.00'
+    # Convert the string of Brazilian currency to float
+    # @return [String] the converted string pattern R$ (unit brazilian) to pattern Float.
     # @raise [TypeError] if the value is not a String
+    # @exemple
+    #   Pombo::Support.str_real_to_float('2,00')
+    #   # => '2.00'
     def self.str_real_to_float(value)
       raise TypeError, "no implicit conversion of #{ value.class.name } into String" unless value.kind_of? String
       value.tr(',','.').to_f
     end
 
-    # return [String] the string representation of a boolean. 'S' to +true+ and 'N' to +false+
+    # Used to convert Boolean values to String
+    # @param [Boolean] the value to be converted
+    # @return [String] the string representation of a boolean. 'S' to +true+ and 'N' to +false+
+    # @exemple
+    #   Pombo::Support.boolean_to_string(true)
+    #   # => 'S'
     def self.boolean_to_string(value)
       value ? 'S' : 'N'
     end
